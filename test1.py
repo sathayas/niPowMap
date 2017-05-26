c77b8ea136e3975325ebcfc02afdec107f4f0b2c
Satoru Hayasaka <hayasaka@utexas.edu>
2017-05-26 16:28:28 -0500

    It runs est_fwhm.py and prepares for est_resel.py

--- test1.py
+++ test1.py
@@ -2,6 +2,13 @@ import numpy as np
 import nibabel as nib
 import sys
 import os
-
+from est_fwhm import est_fwhm
 
 mask_file = 'mask.nii.gz'
+ftstat = 'tstat1.nii.gz'
+df = 6
+stat = 'T'
+
+tstat_img = nib.load(ftstat)
+tstat_data = tstat_img.get_data()
+fwhm_info = est_fwhm(tstat_data, df, stat)
