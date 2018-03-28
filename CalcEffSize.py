11b57ace47a582390cf52084d0684179a889979a
Meg Ashby <megashby@utexas.edu>
2018-03-28 14:10:16 -0500

    Add files via upload

--- CalcEffSize.py
+++ CalcEffSize.py
@@ -1,6 +1,7 @@
 import numpy as np
-from numpy import prod
+# from numpy import prod
 import os
+import math
 
 #I'm not sure what the first line of the Matlab code does, but this is my best guess
 effSize, fOut = CalcEffSize(fStat, fMask, statInfo, FWHM, dirOut)
@@ -16,7 +17,7 @@ else:
 lFWHM = np.array(FWHM).size()
 
 if lFWHM >1:
-	FWHM = prod(FWHM)**(1/lFWHM)
+	FWHM = np.prod(FWHM)**(1/lFWHM)
 
 #directory, file = fileparts(fStat)
 with open(fStat) as f:
@@ -53,9 +54,46 @@ fOutSphere = os.path.join(directory, str("s"+file))
 #another previous function
 SphereConv(fOutNaN, fOutSphere, FWHM)
 
+statHdr, statImg = pm_read_vol(fOutSphere)
+
+if statInfo.type == 'oneT':
+	cohenType = 'd'
+	effSize = np.divide(statImg, math.sqrt(statInfo.N))
+
+elif statInfo.type == 'twoT':
+	cohenType = 'd'
+	effSize = statImg * (math.sqrt(statInfo.N1 + statInfo.N2)/ math.sqrt(statInfo.N1 * statInfo.N2))
+
+elif statInfo.type == 'reg':
+	cohenType = 'd'
+	effSize = np.divide(statImg, statInfo.N)
+
+elif statInfo.type == 'f':
+	cohenType = 'd'
+	effSize = np.multiply((statInfo.df1/statInfo.df2), statImg)
 
 
+if (np.array(mask).size() == 1):
+	mask = np.tile(mask, ((effSize).shape(), (effSize).shape()))
 
+elif np.array(mask).shape() != np.array(effSize).shape():
+	#pm_reslice is another function
+	mask = pm_reslice(mask, effSize.shape())
 
+effSize = np.multiply(mask, effSize)
+
+ # fOut is fStat prepended with d_EffSize_ or f_EffSize
+ #this shouldn't work since os.listdir should take a path not a directory
+if (nargin < 5 or (os.listdir(dirOut) = [])):
+
+	with open(fStat) as f:
+    for line in f:
+        drive, dirOut = os.path.splitdrive(line)
+        dirOut, file = os.path.split(path)
+
+fOut = os.path.join(directory, str(cohenType+"_EffSize_"+file))
+
+#pm_write_vol is another function
+pm_write_vol(statHdr, effSize, fOut)
 
 
