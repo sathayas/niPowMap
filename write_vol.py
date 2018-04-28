946340c9c81cbde42ae5489926b519e24976ceff
Meg Ashby <megashby@utexas.edu>
2018-04-28 17:51:38 -0500

    Create write_vol.py

---
+++ write_vol.py
@@ -0,0 +1,14 @@
+def write_vol(V, X, fName):
+
+	X = np.array(X)
+	data = X
+	img1 = nib.Nifti1Image(data, np.eye(4))
+	img1.header['scl_slope'] = 1
+	img1.header['scl_inter'] = 0
+
+	directory, file = os.path.split(fName)
+	filename_w_ext = os.path.basename(fName)
+	file, ext = os.path.splitext(filename_w_ext)
+	nib.save(img1, os.path.join(directory, filename_w_ext))
+
+	return
