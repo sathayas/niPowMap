3a8ed0e2dc1f1ce4508fe1a8a3994eae574892f2
Meg Ashby <megashby@utexas.edu>
2018-04-28 19:29:57 -0500

    Update SphereConv.py

--- SphereConv.py
+++ SphereConv.py
@@ -1,14 +1,14 @@
-import numpy as np 
 def SphereConv(P, Q, r):
-	#in the actual matlab its eps(7/8) but I can't find comparable func in python
-	tol = np.finfo.eps
+	tol = np.finfo(float).eps
+	tol = tol**(7/8)
 
 	if (isinstance(P, str)):
-		hdr, P = pm_read_vol(P)
+		hdrP, P = read_vol(P)
+
 
 	#isstruct not exist in python
-	if (true):
-		VOX = hdrP.dime.pixdim[1:4]
+	if (hdrP):
+		VOX = hdrP['pixdim'][1:4]
 
 		if VOX[0] < 1:
 			flipdimFlag = 2
@@ -31,33 +31,44 @@ def SphereConv(P, Q, r):
 		Xin = np.flip(Xin, flipdimFlag-1)
 
 	bXin = np.ones(Xin.shape)
+	bXin = bXin.reshape(1,bXin.size)
+	Xin = Xin.reshape(1,bXin.size)
 	tmp = np.argwhere(np.isnan(Xin))
+	tmp = tmp[:, 1]
 	tmp2 = np.argwhere(np.isfinite(Xin))
+	tmp2 = tmp2[:, 1]
 
-	Xin[tmp] = 0
-	bXin[tmp] = 0
 
-	r2 = np.divide(r, VOX)
-	r2ones = ones(r2.shape)
+	for x in np.array(tmp).flat:
+		Xin[0,x] = 0
+		bXin[0, x] = 0
+
+	r2 = np.divide(r, VOX*2)
+	r2ones = np.ones(r2.shape)
 	r2 = np.maximum(r2, r2ones)
 
 	rdim = 2*r2+1
-	roff = np.multiply(r2+1, VOX)
+	roff = np.multiply(r2+1, VOX*2)
 
-	x = meshgrid[0:rdim[0]]
-	y = meshgrid[0:rdim[1]]
-	z = meshgrid[0:rdim[2]]
+	x = np.arange(rdim[0]-1)
+	y = np.arange(rdim[0]-1)
+	z = np.arange(rdim[0]-1)
 
-	x = x*VOX[0]
-	y = y*VOX[1]
-	z = z*VOX[2]
+	x, y, z = np.meshgrid(x, y, z)
+
+	x = x*(VOX[0]*2).T
+	y = y*(VOX[1]*2).T
+	z = z*(VOX[2]*2).T
 
 	xrSphtemp = np.power(x-roff[0], 2)
+	print(xrSphtemp[0, 0, :])
 	yrSphtemp = np.power(y-roff[1], 2)
 	zrSphtemp = np.power(z-roff[2], 2)
 	Sphtemp = xrSphtemp+yrSphtemp+zrSphtemp
 
 	rSph = np.power(Sphtemp, 1/2)
+	
+	return
 
 	brSph = rSph<(r+tol)
 	sumSph = sum(brSph)
@@ -69,13 +80,6 @@ def SphereConv(P, Q, r):
 	XOut[tmp] = np.nan
 
 	pm_write_vol(hdrXin, Xout, Q)
-
-
-
-
-
-
-
 
 
 
